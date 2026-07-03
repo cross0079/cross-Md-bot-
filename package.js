71bd199a8d9a96b3d9a2ecc3fa1269172576ac77
cross0079 <adikaemma6@icloud.com>
2026-07-03 05:42:12 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "cross-md",
+  "version": "1.0.0",
+  "description": "CROSS MD WhatsApp Bot with Telegram Pairing",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "https://github.com/cross0079/cross-Md-bot-",
+    "node-telegram-bot-api": "^0.64.0",
+    "express": "^4.18.2",
+    "canvas": "^2.11.2",
+    "pino": "^8.15.0",
+    "chalk": "^5.3.0",
+    "dotenv": "^16.3.1"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "author": "༄𝐌𝐑.𝐂𝐑𝐎𝐒",
+  "license": "MIT"
+}
